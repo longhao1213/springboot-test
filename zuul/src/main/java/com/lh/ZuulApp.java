2c299262a9f2671cfb9bf39c1924a9857a194318
longhao1213 <1281578910@qq.com>
2020-04-23 16:19:29 +0800

    update

--- zuul/src/main/java/com/lh/ZuulApp.java
+++ zuul/src/main/java/com/lh/ZuulApp.java
@@ -1,16 +1,19 @@
 package com.lh;
 
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * Created by 龙浩 on 2018/7/9.
  */
-//@SpringBootApplication
-//@EnableZuulProxy
-//@MapperScan("com.lh")
-//@EnableFeignClients
+@SpringBootApplication
+@EnableZuulProxy
+@MapperScan("com.lh")
+@EnableFeignClients
 public class ZuulApp {
     public static void main(String[] args){
         SpringApplication.run(ZuulApp.class, args);
